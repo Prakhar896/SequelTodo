f9de89b049eabacdd20351890cc7a1bd6a1d3abf
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-05-08 23:28:20 +0800

    completed create and update of todo completion

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const sequelize = require('sequelize');
 const db = require('./models')
+const { Todo } = require('./models')
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public")); // serves all static files as they are
+app.use(express.urlencoded({ extended: true })); // middleware to parse form data
+app.use(express.json()); // middleware to parse json data
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    Todo.findAll()
+    .then(result => {
+        res.render('index', { data: result })
+    })
+    .catch(err => {
+        res.send(`Error: ${err}`)
+    })
+})
+
+app.post('/create', (req, res) => {
+    var title = req.body['title'];
+    var description = req.body['description'];
+
+    if (!title || !description) {
+        res.send("Invalid parameters")
+        return
+    }
+
+    Todo.create({
+        title: title,
+        description: description,
+        done: false
+    })
+    .then((result) => {
+        res.send("Success")
+        return
+    })
+    .catch(err => {
+        res.send(err);
+    })
+})
+
+app.get('/toggleDone/:id', async (req, res) => {
+    var id = req.params.id
+    if (!id) {
+        res.send("ID not provided.")
+        return
+    }
+
+    const targetTodo = await Todo.findByPk(id)
+    targetTodo.done = !targetTodo.done
+    targetTodo.save()
+    res.redirect('/')
 })
 
 db.sequelize.sync().then((req) => {
